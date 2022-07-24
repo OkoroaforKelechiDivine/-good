0ea49d654596f239ec3780061084784d7ea6cf75
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-07-24 14:05:17 +0100

    styling header components

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,20 +1,31 @@
 import React from "react";
-import img from '../../images/align-left.png';
+import './Header.css'
 
 function Header(){
 
     function showNavLinks() {
-        document.querySelector('.nav-links').classList.toggle('show');
+        document.querySelector('.menu-nav-links').classList.toggle('show');
     }
 
     return (
         <header className="main-header">
             <div className="brand-logo">
-                <h1>WilWa-X</h1>
+                <h2>WilWa-X</h2>
             </div>
-
-            <ul className="nav-links nav-links-1">
+                <ul className="nav-links">
                 {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <li className="nav-link"><a href="#">Blogs</a></li>
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <li className="nav-link"><a href="#">Shops</a></li>
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <li className="nav-link account"><a href="#">Account</a></li>
+                <li className="nav-link menu"><a href="#">Menu</a></li>
+            </ul>
+            <ul className="menu-nav-links">
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <div className="closeButton">
+                    <span>&times;</span>
+                </div>
                 <li className="nav-link"><a href="#">Home</a></li>
                 {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                 <li className="nav-link"><a href="#">About Us</a></li>
@@ -25,18 +36,6 @@ function Header(){
                 {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                 <li className="nav-link"><a href="#"> FAQs</a></li>
             </ul>
-            <ul className="nav-links nav-links-2">
-                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                <li className="nav-link"><a href="#">Menu</a></li>
-                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                <li className="nav-link"><a href="#">Shops</a></li>
-                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                <li className="nav-link account"><a href="#">Account</a></li>
-            </ul>
-
-            <div className="menu">
-                <img onClick={ showNavLinks } src={ img } alt=""/>
-            </div>
         </header>
     )
 }
