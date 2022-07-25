d0557624cc52acb71839eb1fef202b8826180612
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-07-25 10:05:58 +0100

    fixed react typical

--- src/components/heroSection/HeroSection.js
+++ src/components/heroSection/HeroSection.js
@@ -1,5 +1,7 @@
 import React from "react";
+import TypeAnimation from 'react-type-animation'
 import './HeroSection.css'
+import HeroImg from '../../images/pexels-pixabay-373543.jpg'
 import vid from '../../images/mixkit-woman-looking-at-the-horizon-from-a-building-43183-small.mp4'
 
 function HeroSection() {
@@ -19,8 +21,23 @@ function HeroSection() {
                 <h1 className={"hero-text"}>
                     Providing <span>Affordable AI</span> services.<br></br>
                     {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                    <a href={"#"}><button>Get one product here.</button></a>
                 </h1>
+                <button><a>Get one product here.</a></button>
+                <div className="AnimatedText">
+                    <p>
+                        <TypeAnimation
+                            cursor={true}
+                            repeat={Infinity}
+                            sequence={
+                            ['React typing animation based on typical', 2000,
+                                'wilwax', 2000,
+                                'Hello Boy', 2000]}
+                            wrapper="h2"
+                            className="animated-text"
+                        />
+                    </p>
+                </div>
+
             </div>
         </main>
     )
