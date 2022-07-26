c2fbb48bb34058b8ad031623819ec092caf5af2e
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-07-26 09:45:50 +0100

    working on the second section

---
+++ src/components/secondSection/MainSection.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../secondSection/MainSection.css"
+
+function MainSection() {
+    return (
+        <main>
+            <div>
+                <p>Hello</p>
+            </div>
+        </main>
+    )
+}
+export default MainSection;
