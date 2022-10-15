7527c2e2d6499bedbed38b09824328abce797aa7
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-10-15 09:39:54 +0100

    fixed all files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css"
+import About from "./components/About/About";
 
 const App = () => {
     return(
-        <h1>App</h1>
+        <About/>
     )
 }
 
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./skills.scss"
+
+const Skills = () =>{
+    return(
+        <div>About</div>
+    )
+}
+
+export default Skills;
