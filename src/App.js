108ce0dd18fb1b9d41b5f6f28d13d2f1aaf81b9a
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-10-15 07:43:04 +0100

    deleting all files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import Header from "./components/header/header";
-import HeroSection from "./components/heroSection/HeroSection";
-// import MainSection from "./components/secondSection/MainSection";
 
 function App() {
   return (
     <div className={"App"}>
-        <Header />
-        <HeroSection />
-        {/*<MainSection/>*/}
+       <p>Hello</p>
     </div>
   );
 }
