eb23e372dac7d68ea8b0d0994188b2a3dc256541
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-10-15 10:38:36 +0100

    done with default styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
 import "./App.css"
 import About from "./components/About/About";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Skills from "./components/Skills/Skills";
+import Contact from "./components/Contact/Contact";
+import Project from "./components/Project/Project";
 
 const App = () => {
     return(
-        <About/>
+        <>
+            <About/>
+            <Home/>
+            <Navbar/>
+            <Footer/>
+            <Skills/>
+            <Contact/>
+            <Project/>
+        </>
     )
+
 }
 
 export default App;
